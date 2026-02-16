5c71ffb294e8c2c9f72e822c79404ec8692960dc
maliknasir7867 <trustedhackers302@gmail.com>
2026-02-16 14:08:59 +0500

    Create test.js

---
+++ api/test.js
@@ -0,0 +1,8 @@
+module.exports = async (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    return res.status(200).json({ 
+        status: 'ok', 
+        message: 'API is working',
+        timestamp: new Date().toISOString()
+    });
+};
